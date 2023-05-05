11de4d514c0726e4991e588714f5d4704897b773
Charlene Auger <charlene.auger@factorfx.com>
2023-05-05 16:13:17 +0200

    Update field format

--- hook.php
+++ hook.php
@@ -1,20 +1,39 @@
 <?php
 /**
- * @package     gdprcompliance
- * @author      Rudy Laurent
- * @copyright   Copyright (c) 2015-2019 FactorFX
- * @license     AGPL License 3.0 or (at your option) any later version
- *              http://www.gnu.org/licenses/agpl-3.0-standalone.html
- * @link        https://www.factorfx.com
- * @since       2019
+ * ---------------------------------------------------------------------
+ * ITSM-NG
+ * Copyright (C) 2022 ITSM-NG and contributors.
  *
- * --------------------------------------------------------------------------
+ * https://www.itsm-ng.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of ITSM-NG.
+ *
+ * ITSM-NG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ITSM-NG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ITSM-NG. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
  */
 
 /**
- * Plugin install process
+ * plugin_gdprcompliance_install
  *
- * @return boolean
+ * @return void
  */
 function plugin_gdprcompliance_install() {
    global $DB;
@@ -25,7 +44,7 @@ function plugin_gdprcompliance_install() {
                   `date` datetime default NULL,
                   `sucess` int NOT NULL default '0',
                   `error` int NOT NULL default '0',
-                PRIMARY KEY (`id`)
+                  PRIMARY KEY (`id`)
                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
 
       $DB->query($query) or die("error creating glpi_plugin_gdprcompliance_history ". $DB->error());
@@ -36,38 +55,34 @@ function plugin_gdprcompliance_install() {
                   `id` int(11) NOT NULL auto_increment,
                   `active` boolean default FALSE,
                   `mode` int NOT NULL default '0',
-                  `change` VARCHAR(255) default NULL,
-                PRIMARY KEY (`id`)
+                  `change` json default NULL,
+                  PRIMARY KEY (`id`)
                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
 
       $DB->query($query) or die("error creating glpi_plugin_gdprcompliance_configs ". $DB->error());
-   }
 
-   $query = "INSERT INTO `glpi_plugin_gdprcompliance_configs`
-                  VALUES ('1',
-                        'FALSE',
-                        '0', NULL);";
-   $DB->query($query) or die($DB->error());
+      $query = "INSERT INTO `glpi_plugin_gdprcompliance_configs` VALUES ('1', 'FALSE', '0', NULL);";
+      $DB->query($query) or die($DB->error());
+   }
 
    CronTask::Register('PluginGdprcomplianceAction', 'CleanUsers', DAY_TIMESTAMP);
 
    return true;
 }
 
-
 /**
- * Plugin uninstall process
+ * plugin_gdprcompliance_uninstall
  *
- * @return boolean
+ * @return void
  */
 function plugin_gdprcompliance_uninstall() {
    global $DB;
    
-   // Old version tables
    if ($DB->tableExists("glpi_plugin_gdprcompliance_history")) {
       $query = "DROP TABLE `glpi_plugin_gdprcompliance_history`";
       $DB->query($query) or die("error deleting glpi_plugin_gdprcompliance_history");
    }
+
    if ($DB->tableExists("glpi_plugin_gdprcompliance_configs")) {
       $query = "DROP TABLE `glpi_plugin_gdprcompliance_configs`";
       $DB->query($query) or die("error deleting glpi_plugin_gdprcompliance_configs");
